9b5c937571322d8fc6568deb50e3dcc4e7880add
Jason Adams <jason@yellowgorilla.net>
2014-04-25 18:02:54 -0300

    Cleanup initial commit mess

--- README.md
+++ README.md
@@ -6,9 +6,14 @@
 ## How to use it
 
 You must include the validate-abn-directive dependency on your angular module:
+
     var app = angular.module("demoapp", ["validate-abn-directive"]);
+
 After that, you simply add the "validate-abn" attribute in the input you would like to validate
+
     <input name="abn" ng-model="abn" validate-abn>
+
 You can then check whether the value failed this particular test by checking the "validAbn" property of $error object.
  e.g.
+
     ng-class="{invalid-abn: myForm.abn.$error.validAbn}"
---
+++ validate-abn-directive.js
@@ -0,0 +1,41 @@
+angular.module('validate-abn-directive',[])
+  .directive('validateAbn', function() {
+    'use strict';
+    return {
+      require: 'ngModel',
+      link: function(scope, elm, attrs, ctrl) {
+
+        var weights = [10, 1, 3, 5, 7, 9, 11, 13, 15, 17, 19];
+
+        // watch the scope array that contains the list of invalid values for changes
+
+        // Call the "checkValue" called whenever the value in the element changes
+        ctrl.$parsers.unshift(checkValue);
+
+        // This is the function that does the validation
+        function checkValue(viewValue) {
+          // strip anything other than digits
+          var abn = viewValue.replace(/[^0-9]+/g, '');
+
+          // abn is always 11 digits long
+          if (abn.length === 11) {
+
+            // apply ato check method
+            var sum=0;
+            var digit = null;
+            for (var i=0; i<weights.length; i++) {
+              digit = parseInt(abn.substring(i,i+1) - (i ? 0 : 1),10);
+              sum += weights[i] * digit;
+            }
+            if (sum%89 === 0) {
+              ctrl.$setValidity('validAbn', true);
+              return viewValue;
+            }
+          }
+          ctrl.$setValidity('validAbn', false);
+          return viewValue;
+        }
+      }
+    };
+  }
+);
